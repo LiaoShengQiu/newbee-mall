f2246acabd955a85ee96741b2ffd5777880a6eed
LiaoShengQiu <58867918+LiaoShengQiu@users.noreply.github.com>
2020-02-11 15:14:55 +0800

    更新了一些文件位置

--- src/main/java/com/example/newbeemall/NewbeeMallApplication.java
+++ src/main/java/com/example/newbeemall/NewbeeMallApplication.java
@@ -1,9 +1,11 @@
 package com.example.newbeemall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.newbeemall.mapper")
 public class NewbeeMallApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/newbeemall/controller/TbNewbeeMallAdminUserController.java
+++ src/main/java/com/example/newbeemall/controller/TbNewbeeMallAdminUserController.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Controller;
  * @since 2020-02-07
  */
 @Controller
-@RequestMapping("/tbNewbeeMallAdminUser")
+@RequestMapping("/admin")
 public class TbNewbeeMallAdminUserController {
 
 }
