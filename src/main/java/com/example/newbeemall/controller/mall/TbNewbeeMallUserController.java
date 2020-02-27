3f02982caec79c6ec81bb41819baf77f54714b0f
Mr.黄 <1262199083@qq.com>
2020-02-27 17:01:23 +0800

    终于改好了  订单1

--- src/main/java/com/example/newbeemall/controller/mall/TbNewbeeMallUserController.java
+++ src/main/java/com/example/newbeemall/controller/mall/TbNewbeeMallUserController.java
@@ -129,7 +129,7 @@ if(request.getSession().getAttribute("code")!=null){
                     int count = cartItemService.getCartCountByUserId(list.get(0).getUserId());
                     list.get(0).setShopCartItemCount(count);
                     request.getSession().setAttribute("newBeeMallUser",list.get(0));
-                    System.out.println("登录成功！");
+                    System.out.println("登录成功！！！");
                 }else{
                     System.out.println("密码错误！");
                     msg = "密码错误！";
